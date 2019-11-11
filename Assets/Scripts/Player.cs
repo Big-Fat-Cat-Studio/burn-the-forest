ca39e0b1eaa238db9a5c73e0676cc523b31c15c6
Hamza Tokuchi <h@mza.jp>
2019-11-11 09:31:01 +0100

    Object interaction - Object spead

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody MyRigidbody;
+    Vector3 Velocity;
+
+
+    void Start()
+    {
+        MyRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        Vector3 keyInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 forward = keyInput.normalized;
+
+        Velocity = forward * 10f;
+    }
+
+
+    void FixedUpdate()
+    {
+        MyRigidbody.position += Velocity * Time.fixedDeltaTime;
+
+    }
+}
